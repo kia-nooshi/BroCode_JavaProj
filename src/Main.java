11a8ef456ca6c9dc135ac9667751889057d57daf
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-17 16:25:50 -0700

    3 print statements!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
-
+        System.out.print("I'm so behind on this shit! \n");
+        System.out.println("sout is a shortcut to get the newline print statement !");
 
     }
 }

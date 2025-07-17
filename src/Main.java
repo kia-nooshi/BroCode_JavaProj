6258beea5b7ab37b66ba8406f9d9b3d6dabdff49
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-17 16:21:04 -0700

    First Java Project with tutorial from BroCode

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This is my first time here.");
+        System.out.print("I'm so behind on this shit!");
+
+
+    }
+}

2b70f04c39fa7aadb454503232657b4e13f77e71
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-18 15:10:47 -0700

    Mad Libs Game

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        //MAD LIBS GAME
+        Scanner input = new Scanner(System.in);
+
+        String adj1;
+        String noun1;
+        String adj2;
+        String verb1;
+        String adj3;
+
+        System.out.print("Enter an adjective: ");
+        adj1 = input.nextLine();
+        System.out.print("Enter a noun: ");
+        noun1 = input.nextLine();
+        System.out.print("Enter an adjective: ");
+        adj2 = input.nextLine();
+        System.out.print("Enter a verb end with -ing: ");
+        verb1 = input.nextLine();
+        System.out.print("Enter an adjective: ");
+        adj3 = input.nextLine();
+
+        System.out.println("\nToday I went to a " + adj1 + "zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adj2 + " and " + verb1 + "!");
+        System.out.println("I was " + adj3 + "!");
+
+        input.close();
+
 
 
     }

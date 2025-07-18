727cc8c644aa399d9f300eedd8151143579f8f91
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-18 15:44:14 -0700

    Arithmetic Operators, Augmented Assignment Operators, Increment and Decrement Operators, ORDER OF OPERATIONS [P-E-M-D-A-S]

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,87 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        //Arithmetic Operators
+        //int x = 10;
+        int y = 2;
+        int z;
+
+        /*addition use +
+        z=x + y;
+        System.out.println(z);
+        */
+
+        /* subtraction use -
+        z=x - y;
+        System.out.println(z);
+         */
+
+        /* multiplication use *
+        z=x * y;
+        System.out.println(z);
+         */
+
+        /* division use /
+        z=x / y;
+        System.out.println(z);
+         */
+
+        /* modulus use % this gives the whole # remainder of any division with z storing the remainder
+        useful for determining even or odd and working with time to see if its divisible by sec or min
+        z=x % y;
+        System.out.println(z);
+         */
+
+        //-----------------------------------
+
+        //Augmented Assignment Operators
+        /* += is the same as x = x + y
+        x += y;
+        System.out.println(x);
+         */
+
+        /* -= is the same as x = x - y
+        x -= y;
+        System.out.println(x);
+         */
+
+        /* *= is the same as x = x * y
+        x *= y;
+        System.out.println(x);
+         */
+
+        /* /= is the same as x = x - y
+        x /= y;
+        System.out.println(x);
+        //for int division the remainder decimal does not print out nor the remainder
+        //need to use doubles for that
+         */
+
+        /* %= is the same as x = x % y
+        x %= y;
+        System.out.println(x);
+         */
+
+        //-----------------------------
+        /*
+        //Increment and Decrement Operators
+        int x = 1;
+
+        x++; //use this to increment x by 1
+        x--; //use this to decrement x by 1
+
+
+        System.out.println(x);
+         */
+
+        //-----------------------------
+
+        //ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7-5) /2.0;
+
+        System.out.println(result);
+
+
 
 
     }

696dc6d5f4ccebee7213761133dec70fe2eca115
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-19 15:58:40 -0700

    Shopping cart project calculating the total of buying an item

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        //shopping cart program
+        Scanner scanner = new Scanner(System.in);
 
+        String item;
+        double price;
+        int quantity;
+        //can initialize a char currency = '$' if we want but is unnecessary
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each " + item + "?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many " + item + "'s would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.println("You have bought " + quantity + " " + item + "/s.");
+        System.out.println("Your total is $" + total);
+
+
+        //don't forget to close the scanner
+        scanner.close();
 
     }
 }

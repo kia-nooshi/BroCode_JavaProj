73bfb7ffb427f04595e91f638038690a5f6f2294
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-25 15:13:50 -0700

    Built a compound interest calculator!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        //compound interest calculator
+        //Compound interest formula = P[1+(r/100/n)]^nt
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        double compound;
+        int time;
+        double investment;
+
+        System.out.print("Enter principal amount: ");
+        principal = scanner.nextDouble();
+        System.out.print("Enter rate amount (in %): ");
+        rate = scanner.nextDouble();
+        // can also be rate = scanner.nextDouble()/100;
+        System.out.print("Enter the amount of times the interest compounds yearly: ");
+        compound = scanner.nextInt();
+        System.out.print("Enter the number of years: ");
+        time = scanner.nextInt();
+
+        investment = principal* (Math.pow((1 + ((rate/100) / compound)), (compound * time)));
+        if (time == 1) {
+            System.out.printf("Investment after " + time + " year is $%.2f%n", investment);
+        }else{
+            System.out.printf("Investment after " + time + " years is $%.2f%n", investment);
+        }
+
+        scanner.close();
+
 
 
     }

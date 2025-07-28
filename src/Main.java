a6d47144ecc847157aed8a395d9dc379876037c7
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-28 14:33:22 -0700

    Worked with nested if-statements and built a discount calculator based on Booleans.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        //possible to have if statements within if statements
+        //can have if, else if, else statements
+
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+
+
+
+        if(isStudent){
+            if (isSenior){
+                System.out.println("You get a Senior discount of 20%");
+                System.out.println("You get a Student discount of 10%");
+                price *= 0.8;
+                price *= 0.9;
+            }else {
+                System.out.println("You get a Student Discount of 10%");
+                price *= 0.9;
+            }
+
+        } else{
+            if (isSenior){
+                System.out.println("You get a Senior discount of 20%");
+                price *= 0.8;
+            }
+            price *=1;
+
+        }
+
+        System.out.printf("The price of a ticket is: $%.2f" ,price);
+
+
 
 
     }

fe052de6b1de4e634cf13b2aeb301dbaaec7c5d6
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-30 15:38:24 -0700

    Weight conversion program with menus, choices, and if-statements to hold the logic of converting kgs to lbs and vice versa.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        //weight conversion program
+
+        //declare vars
+        double kgs;
+        double pounds;
+
+        int option;
+        double weight;
+        double convWeight;
+
+        //welcome and prompt menus
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2: Convert kgs to lbs");
+        System.out.print("Please choose an option: ");
+        Scanner scanner = new Scanner(System.in);
+        option = scanner.nextInt();
+
+        //if statements with options 1,2, and invalid
+        if (option == 1){
+            System.out.print("Please enter your weight in lbs: ");
+            weight = scanner.nextDouble();
+            convWeight = weight * 0.453592;
+            System.out.printf("Your weight in kgs is: %.2f", convWeight);
+
+        }else if (option ==2){
+            System.out.print("Please enter your weight in kgs: ");
+            weight = scanner.nextDouble();
+            convWeight = weight * 2.2046;
+            System.out.printf("Your weight in lbs is: %.2f", convWeight);
+        }else {
+            System.out.println("Not a valid option");
+        }
+
 
 
     }

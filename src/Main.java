7dc8afb779ab14b9a801f93e3510bb5b8838a481
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-31 21:28:27 -0700

    Learned of the ternary operator which follows the following format: variable = (condition) ? ifTrue : ifFalse; can be used as a replacement for if statements

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
 
+        //ternary operator (question mark followed by 1 of 2 values) ? = Return 1 of 2 values if a condition is true
+        //variable = (condition) ? ifTrue : ifFalse;
+        //simpler version of an if else statement
+
+        int score = 70;
+
+        /*
+        if (score >= 60){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+        } */ //regular if statement, using a ternary operator we can rewrite this
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        // Data type initialized with a condition followed by a ? and two outcomes (if true : if false)
+
+        System.out.println(passOrFail);
+
+        int number = 3;
+
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+
+        System.out.println(evenOrOdd);
+
+        int hours = 13;
+
+        String timeOfDay = (hours < 12) ? "AM" : "PM";
+        System.out.println(timeOfDay);
+
+        int income = 60000;
+
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.println(taxRate);
 
     }
 }

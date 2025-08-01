00b12d4d66d62b4e491f6017377d21c48a4476b6
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-01 13:07:22 -0700

    Learned about enhanced switches which can be done to replace many if else statements.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import javax.xml.transform.Source;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        //Enhanced switch = a replacement to many else if statements
+        //(Java 14 feature)
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the day of the week: ");
+        String day = scanner.nextLine();
+        //arrow operator means do something
+        switch(day){
+            /* case "Monday" -> System.out.println("It is a weekday");
+            case "Tuesday" -> System.out.println("It is a weekday");
+            case "Wednesday" -> System.out.println("It is a weekday");
+            case "Thursday" -> System.out.println("It is a weekday");
+            case "Friday" -> System.out.println("It is a weekday");
+            case "Saturday" -> System.out.println("It is a weekend");
+            case "Sunday" -> System.out.println("It is a weekend"); */
+            //can comma separate the cases above to combine them into one case
+            case "Monday","Tuesday", "Wednesday", "Thursday", "Friday"  -> System.out.println("It is a Weekday");
+            case "Saturday", "Sunday"  -> System.out.println("It is the Weekend");
+            default -> System.out.println(day + " is not a day"); //default behaves similarly to an else statement
+        }
 
     }
 }

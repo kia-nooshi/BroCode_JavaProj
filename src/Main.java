470daa4a7fbbc6aec31bd0eeb5715c7932e7f6ee
kia-nooshi <nooshikianoosh@gmail.com>
2025-07-31 22:11:05 -0700

    Created a temperature converter using the ternary operator, if statements, and printf method. Also utilized method chaining to convert the unit to lower case.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temperature;
+        String tempMeasurement;
+        double convertedTemp;
+
+        System.out.print("Enter the temperature: ");
+        temperature = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        tempMeasurement = scanner.next().toLowerCase();; //can use method chaining to make it upper/lower case from here
+        //original line: tempMeasurement = scanner.next();
+
+        //tempMeasurement = tempMeasurement.toLowerCase();
+        if((!tempMeasurement.equals("c") && !tempMeasurement.equals("f"))){
+            System.out.println("Not a Valid Measurement");
+        }else {
+
+            convertedTemp = (tempMeasurement.equals("c")) ? (temperature - 32) * 5 / 9 : (temperature * 9 / 5) + 32;
+
+            tempMeasurement = tempMeasurement.toUpperCase();
+            System.out.printf("%.1f° %s", convertedTemp, tempMeasurement);
+
+        }
+
+        scanner.close();
+
 
 
     }

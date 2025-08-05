7af5a2f4f351e8c2f8f1cf781eb17cd1de41b283
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-05 14:28:21 -0700

    Learned about nested loops. A very important portion of DSA algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        Scanner scanner = new Scanner(System.in);
+        //nested loop = A loop inside another loop
+        // used often with DS&A
+
+//        for (int j = 1; j <= 3; j++) {
+//            for (int i = 1; i <= 9; i++) {
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //matrix of a symbols
+        System.out.print("Enter the # of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the # of columns: ");
+        int columns = scanner.nextInt();
+        System.out.print("Enter the symbol to use: ");
+        String symbol = scanner.next();
+
+        for (int j = 1; j <= rows; j++) {
+            for (int i = 1; i <= columns; i++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+
+
+        scanner.close();
+
+
 
 
     }

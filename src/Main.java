b51d6980744b07aab987b6d55a0578f283c774f3
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-04 22:48:42 -0700

    Learned break and continue keywords. break = break out of a loop (STOP). continue = skip current iteration of a loop (SKIP)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+
+        //break = break out of a loop (STOP)
+        //continue = skip current iteration of a loop (SKIP)
+
+        for (int i = 0; i < 10; i++){
+
+            if (i == 5){
+                continue; // prints 0 1 2 3 4 6 7 8 9
+                //break prints 0 1 2 3 4
+            }
+            System.out.print(i + " ");
+        }
 
 
     }

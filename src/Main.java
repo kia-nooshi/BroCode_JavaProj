31d34871623576e6784dc998de74aed047859022
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-04 22:43:37 -0700

    Learned for Loops which executes some code a certain amount of times. For Loop is broken into 3 steps. initialization, condition, counter

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+    public static void main(String[] args) throws InterruptedException{
+
+
+        //for loop = execute some code a CERTAIN amount of times
+
+        Scanner scanner = new Scanner(System.in);
+        //3 statements seperated by semi-colons
+        //1st statement is a counter which is generally made to be i. also called a loop control variable
+        //2nd statement is a condition, when we want to stop
+        //3rd statement is the step statement. We can increment and decrement the counter
+        //can increment or decrement by any size of a number
+        //initialization, condition, counter
+
+//        for(int i = 2; i < 10 ; i++ ){
+//            System.out.println(i);
+//
+//        }
+
+        System.out.print("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for(int i = 1; i < max +1; i++){
+            System.out.println(i);
+        }
+
+        int start = 10;
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("HAPPY NEW YEAR!");
+
+        scanner.close();
 
 
     }

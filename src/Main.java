124ecd79025e631aacf7d8b4d94c6517ed1838c2
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-05 22:38:28 -0700

    Learned about variable scope. Local scopes declared in methods/curly brackets. Other methods not aware of other local vars. Class vars can be accessed freely from other methods.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+
 public class Main {
+
+    static int x = 3;//CLASS scope which can be accessed from the other methods
+    //var is outside in the street
+    //Local variables that share the same name as a class variable will take precedent
+    //Class vars are useful with Object oriented programming and/or when we have constants
+
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        //variable scope = where a variable can be accessed
 
-
+        //inside of a method if you declare a variable it has a local scope
+        //it won't be recognized in another method
+        int x = 1; //LOCAL
+        System.out.println(x);
+        doSomething();
+    }
+    static void doSomething(){
+        int x = 2;//vars can have same names in different methods since they have the local scopes
+        System.out.println(x);
     }
 }

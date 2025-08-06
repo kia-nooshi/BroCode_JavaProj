2b88472f14aa00c8239bc800d2091ad914ffd4a7
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-05 22:09:31 -0700

    Learned about overloaded methods. Methods can share names but not parameters. method name + Parameters = its signature.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
 
+        //overloaded methods = methods that share the same name, but different parameters
+        //unique method signature = name + Parameters;
+        //no two methods can share the same signatures
+//        System.out.println(add(1,2, 3,4));
+//        System.out.println(add(1,2,3));
+//        System.out.println(add(1,2));
 
+        String pizza = bakePizza("flat bread", "mozzarella", "pepperoni");
+
+        System.out.println(pizza);
     }
+    static String bakePizza(String bread ){
+        return bread + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese ){
+        return cheese + " " + bread + " pizza";
+    }
+
+    static String bakePizza(String bread, String cheese, String topping ){
+        return topping + " " + cheese + " " + bread + " pizza";
+    }
+
+
+//    static double add(double a, double b){
+//        return a + b;
+//    }
+//    //no two methods can share the same signature, they can have the same name but not same params
+//    static double add(double a, double b, double c){
+//        return a + b + c;
+//    }
+//    static double add(double a, double b, double c, double d){
+//        return a + b + c + d;
+//    }
+
+
 }

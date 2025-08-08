17de2c24f926760cd75382f9dcf13d0500d29035
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-08 08:46:16 -0700

    Learned how to take in inputs for arrays and how to iterate through them.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
 
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
+
+        System.out.print("What number of food do you want?: ");
+        size = scanner.nextInt();
+        //create empty array with the size of the array before filling in the values
+        foods = new String[size];
+        scanner.nextLine();
+
+        for(int i = 0; i < foods.length; i++){
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food : foods){
+            System.out.println(food);
+        }
+
+
+
+
+        scanner.close();
 
     }
 }

283850d7f1a0e8d909a668eb4cc8b34b7a6d1818
kia-nooshi <nooshikianoosh@gmail.com>
2025-08-08 07:58:45 -0700

    Dice Rollling program using for loops and enhanced switches located in methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,83 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static int total = 0;
     public static void main(String[] args){
-        System.out.println("This is my first time here.");
-        System.out.print("I'm so behind on this shit!");
+        Random random = new Random();
+        int numRolls;
+        int dicenum;
 
+        System.out.print("Enter the number of dice to roll: ");
+        numRolls = scanner.nextInt();
+
+
+        if (numRolls <= 0){
+            System.out.println("You can't 0 dice!");
+        }else{
+            System.out.printf("Rolling %d dice... \n", numRolls);
+            for (int i = 0; i < numRolls; i++){
+                dicenum = random.nextInt(1,7);
+                diceRoll(dicenum);
+                System.out.printf("You rolled: %d \n", dicenum);
+                total += dicenum;
+
+            }
+            System.out.printf("Total of all rolls: %d \n", total);
+        }
+
+
+
+        scanner.close();
 
     }
+    static void diceRoll(int dicenum){
+        switch (dicenum) {
+            case 1 -> {
+                System.out.println(" _____ ");
+                System.out.println("|     |");
+                System.out.println("|  ●  |");
+                System.out.println("|     |");
+                System.out.println(" ----- ");
+            }
+            case 2 -> {
+                System.out.println(" _____ ");
+                System.out.println("| ●   |");
+                System.out.println("|     |");
+                System.out.println("|   ● |");
+                System.out.println(" ----- ");
+            }
+            case 3 -> {
+                System.out.println(" _____ ");
+                System.out.println("| ●   |");
+                System.out.println("|  ●  |");
+                System.out.println("|   ● |");
+                System.out.println(" ----- ");
+            }
+            case 4 -> {
+                System.out.println(" _____ ");
+                System.out.println("|●   ●|");
+                System.out.println("|     |");
+                System.out.println("|●   ●|");
+                System.out.println(" ----- ");
+            }
+            case 5 -> {
+                System.out.println(" _____ ");
+                System.out.println("|●   ●|");
+                System.out.println("|  ●  |");
+                System.out.println("|●   ●|");
+                System.out.println(" ----- ");
+            }
+            case 6 -> {
+                System.out.println(" _____ ");
+                System.out.println("|●   ●|");
+                System.out.println("|●   ●|");
+                System.out.println("|●   ●|");
+                System.out.println(" ----- ");
+            }
+            default -> System.out.println("Invalid roll!");
+        }
+
+    }
+
 }
